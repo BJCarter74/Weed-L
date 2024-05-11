fcb21ce3502a6a84677756bb2631f646241f481e
BJCarter74 <brodrick.carter74@gmail.com>
2024-05-11 18:37:30 -0500

    frontend focus

---
+++ Weed-L/src/pages/Sessions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SessionList from "../components/SessionsList";
+import "../css/SessionList.css";
+const Sessions = () => {
+  return (
+    <div className="session-pane">
+      <SessionList />
+    </div>
+  );
+};
+
+export default Sessions;
---
+++ Weed-L/src/pages/Strains.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StrainsList from "../components/StrainList";
+import "../css/StrainList.css";
+const StrainPage = () => {
+  return (
+    <div className="strain-view">
+      <StrainsList />
+    </div>
+  );
+};
+
+export default StrainPage;
