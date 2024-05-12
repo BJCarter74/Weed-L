1435870e4184fb6fac76922b95feb82b3900320b
BJCarter74 <brodrick.carter74@gmail.com>
2024-05-12 09:37:43 -0500

    ughh

--- Weed-L/src/api/axiosConfig.js
+++ Weed-L/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://weed-l-backend-f6215667c3a8.herokuapp.com/",
+  baseURL: "https://weed-l-backend-f6215667c3a8.herokuapp.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
