2e67fa9042a4fcd3497f7dce9f530d37e22049b8
BJCarter74 <brodrick.carter74@gmail.com>
2024-05-12 10:36:10 -0500

    ughhhh

--- Weed-L/src/components/StrainList.jsx
+++ Weed-L/src/components/StrainList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../api/axiosConfig.js";
 import "../css/StrainList.css";
 
 const StrainsList = () => {
